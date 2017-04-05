61db6dbe344ee0367b1165099c1498d877766652
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-04-05 17:57:27 +0530

    Phase1 of NPL

--- NLPConsole/NLPConsole/Program.cs
+++ NLPConsole/NLPConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLPConfiguration;
 
 namespace NLPConsole
 {
@@ -10,7 +11,22 @@ namespace NLPConsole
     {
         static void Main(string[] args)
         {
+            NaturalLanguageProcessor nlp = new NaturalLanguageProcessor();
+            nlp.LoadConfigurationFile(@"C:\Users\Harshvardhan.Poddar\Documents\NaturalLanguageProcessor\NLPConfiguration.json");
 
+            while(true)
+            {
+                Console.WriteLine("Enter your Input");
+                string userSearch = Console.ReadLine();
+                var intentResult = nlp.GetMatchingIntent(userSearch);
+
+
+                if (intentResult != null)
+                    Console.Write("Awesome, I will get it done. Action: " + intentResult.Action);
+                else
+                    Console.Write("Sorry, I do not understand that.");
+                Console.ReadLine();
+            }
         }
     }
 }
